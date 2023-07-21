4c66cfce83b668071ee7912fc45a4290b3cb20c1
LordBrandon07 <brandonnrodriguez07@gmail.com>
2023-07-21 12:15:32 -0500

    firstcomments

--- helloWorld/src/main/variables.java
+++ helloWorld/src/main/variables.java
@@ -8,8 +8,29 @@ public class variables {
 		System.out.println(numero);
 		numero = 10;                   //se puede modificar el valor n veces
 		System.out.println(numero);
+		
+		float flotante = 9.8f; // tiene que terminar con f
+		
+		char letra = 'a'; // comillas simples
+		char number = 97; //
+		System.out.println(letra); // char es caracter de tipo especial
+		System.out.println(number);
+		
+		boolean verdad = true;
+		
+		int var1 =1, var2=2, var3=3;
+		
+		System.out.print(var1+"\n"); // con \n salto de linea
+		System.out.println(var2); //print sin nada mas no da salto de lian pero println es salto de linea
+		System.out.println(var3);
+		
+		final float pi =3.1416f; // con final es una constante 
+		System.out.println(pi);
+		
+		var1++; // incrementar en 1
+		System.out.println(var1);
+		var2 += 5;
+		System.out.println(var2);
 	}
-
-	
 
 }
