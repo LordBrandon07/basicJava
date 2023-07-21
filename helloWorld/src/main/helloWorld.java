c46be8ad84f078001158af10851aa35d102142a0
LordBrandon07 <brandonnrodriguez07@gmail.com>
2023-07-20 22:04:53 -0500

    firsteclipse

---
+++ helloWorld/src/main/helloWorld.java
@@ -0,0 +1,15 @@
+package main;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hola Mundo"); //hello world
+		
+		/*
+		 * viva colombia,
+		 * viva falcao
+		 * */
+	}
+
+}
---
+++ helloWorld/src/main/variables.java
@@ -0,0 +1,15 @@
+package main;
+
+public class variables {
+
+	public static void main(String[] args) {
+		
+		int numero = 8;
+		System.out.println(numero);
+		numero = 10;                   //se puede modificar el valor n veces
+		System.out.println(numero);
+	}
+
+	
+
+}
